0a6e5a2a9b120861df66d6e25ed1709303e30c81
Yasin2719 <karakusyasin19@gmail.com>
2024-02-19 22:23:15 +0100

    feat(abstractions): add baseEntity class

---
+++ src/Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Abstractions;
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+}
+
