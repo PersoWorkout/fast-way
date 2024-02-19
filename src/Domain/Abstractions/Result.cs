83d28f49e754dc515894c8d54b291d20ae0bd776
Yasin2719 <karakusyasin19@gmail.com>
2024-02-19 22:42:02 +0100

    feat(abstraction): added result & error classes

---
+++ src/Domain/Abstractions/Result.cs
@@ -0,0 +1,37 @@
+﻿namespace Domain.Abstractions
+{
+    public class Result<T> where T : class?
+    {
+        private Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None ||
+                !isSuccess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(error));
+            }
+            IsSucess = isSuccess;
+            Error = error;
+        }
+
+        private Result(T? data = null)
+        {
+
+            IsSucess = true;
+            Error = Error.None;
+            Data = data;
+        }
+
+        public bool IsSucess { get; }
+
+        public bool IsFailure => !IsSucess;
+
+        public Error Error { get; }
+
+        public T? Data { get; }
+
+        public static Result<T> Success(T data) => new(data);
+        public static Result<T> Success() => new();
+
+        public static Result<T> Failure(Error error) => new(false, error);
+    }
+}
