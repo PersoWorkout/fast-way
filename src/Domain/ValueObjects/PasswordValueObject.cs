ac4ad235ae954e64cc6d7fe0cf46a1a4d37e9bb9
Yasin2719 <karakusyasin19@gmail.com>
2024-02-19 23:52:38 +0100

    feat(passwordValueobject): add & test password valueObject

---
+++ src/Domain/ValueObjects/PasswordValueObject.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Abstractions;
+using System.Text.RegularExpressions;
+
+namespace Domain.ValueObjects;
+
+public class PasswordValueObject : ValueObject
+{
+    private const string PasswordRegexPattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[~`!@#\$%\^&\*\(\)_\-\+=\{\[\}\]\|\\:;'<,>\.\?\/]).{8,}$";
+    private const RegexOptions PasswordRegexOptions = RegexOptions.None;
+    public string Value { get; set; }
+
+    private PasswordValueObject(string value) 
+    {
+        Value = value;
+    }
+
+    public static Result<PasswordValueObject> Create(string value)
+    {
+        List<Error> errors = [];
+        if (string.IsNullOrEmpty(value))
+            errors.Add(new Error("Password.Empty", "The password is required"));
+
+        if (!Regex.IsMatch(value,
+                PasswordRegexPattern,
+                PasswordRegexOptions,
+                TimeSpan.FromMilliseconds(250)))
+            errors.Add(new Error("Password.Invalid", "The password don't respect the format"));
+
+        if (errors.Count > 0)
+            return Result<PasswordValueObject>.Failure(errors);
+
+        return Result<PasswordValueObject>
+            .Success(new PasswordValueObject(value));
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/Domain.UnitProject/ValueObjects/PasswordValueObjectTest.cs
@@ -0,0 +1,40 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UnitProject.ValueObjects
+{
+    public class PasswordValueObjectTest
+    {
+        [Fact] 
+        public void Create_ShouldReturnSucessResult_WhenValueIsCorrectPassword()
+        {
+            //Arrange
+            const string PasswordValue = "Password123!";
+
+            //Act
+            var valueObject = PasswordValueObject.Create(PasswordValue);
+
+            //Assert
+            Assert.True(valueObject.IsSucess);
+            Assert.Equal(PasswordValue, valueObject.Data.Value);
+        }
+
+        [Fact]
+        public void Create_ShouldReturnFailureResult_WhenValueIsIncorrectPassword()
+        {
+            //Arrange
+            const string PasswordValue = "test!";
+
+            //Act
+            var valueObject = PasswordValueObject.Create(PasswordValue);
+
+            //Assert
+            Assert.True(valueObject.IsFailure);
+            Assert.NotEmpty(valueObject.Errors);
+        }
+    }
+}
