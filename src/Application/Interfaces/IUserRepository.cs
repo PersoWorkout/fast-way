e0c4962f07aeeb214f631acab9562c0e814271c8
Yasin2719 <karakusyasin19@gmail.com>
2024-02-20 22:34:15 +0100

    feat(user): create user repository interface

---
+++ src/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> Create(User user);
+        Task<User> GetById(Guid id);
+        Task<bool> ExistEmail(string Email);
+        Task<User> Update(User user);
+        Task<bool> Delete(User user);
+    }
+}
