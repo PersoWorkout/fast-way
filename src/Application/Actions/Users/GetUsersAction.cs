d6d84785e1ac4979d35f8c7d3c4b3ddebd186100
Yasin2719 <karakusyasin19@gmail.com>
2024-02-20 22:54:01 +0100

    feat(users): create GetUsers action

---
+++ src/Application/Actions/Users/GetUsersAction.cs
@@ -0,0 +1,17 @@
+﻿using Application.Queries;
+using Domain.Abstractions;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Actions.Users
+{
+    public class GetUsersAction(IMediator mediator)
+    {
+        private readonly IMediator _mediator = mediator;
+
+        public async Task<Result<List<User>>> Execute()
+        {
+            return await _mediator.Send(new GetUsersQuery());
+        }
+    }
+}
