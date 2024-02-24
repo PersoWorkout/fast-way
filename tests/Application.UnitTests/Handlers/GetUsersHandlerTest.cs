e57e173deca7da2889fd55b101fefba8c0776122
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 00:45:58 +0100

    feat(users): add GetUsersHAndler test

---
+++ tests/Application.UnitTests/Handlers/GetUsersHandlerTest.cs
@@ -0,0 +1,44 @@
+﻿using Application.Handlers.Users;
+using Application.Interfaces;
+using Application.Queries;
+using Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Handlers
+{
+    public class GetUsersHandlerTest
+    {
+        private Mock<IUserRepository> _mockedUserRepository;
+
+        public GetUsersHandlerTest()
+        {
+            _mockedUserRepository = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnSuccessResult()
+        {
+            //Arrange
+            var query = new GetUsersQuery();
+
+            _mockedUserRepository.Setup(
+                x => x.GetAll())
+                .ReturnsAsync([]);
+
+            var handler = new GetUsersHandler(_mockedUserRepository.Object);
+
+            //Act
+
+            var result = await handler.Handle(query, default);
+
+            //Assert
+            Assert.True(result.IsSucess);
+            Assert.IsType<List<User>>(result.Data);
+        }
+    }
+}
