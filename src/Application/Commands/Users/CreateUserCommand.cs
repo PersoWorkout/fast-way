5b36454605457e54d147eeff8291faeebcd1ea8a
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 14:34:41 +0100

    feat(application.user): add create user command

---
+++ src/Application/Commands/Users/CreateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Abstractions;
+using Domain.DTOs.Users.Response;
+using Domain.ValueObjects;
+using MediatR;
+
+namespace Application.Commands.Users
+{
+    public class CreateUserCommand: IRequest<Result<UserForList>>
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public EmailValueObject Email { get; set; }
+        public PasswordValueObject Password { get; set; }
+    }
+}
