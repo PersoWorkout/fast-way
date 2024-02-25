57df68881fc4f274aa18ffcebcbe2d419b70e40a
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 10:35:37 +0100

    feat(application.user): add & test GetUserById handler

---
+++ src/Application/Handlers/Users/GetUserByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Application.Queries;
+using Domain.Abstractions;
+using Domain.Errors;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Handlers.Users
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, Result<User>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Result<User>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetById(request.Id);
+            if (user is null)
+                return Result<User>.Failure(
+                    UserErrors.NotFound(request.Id.ToString()));
+
+            return Result<User>.Success(user);
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Handlers/GetUserByIdHandlerTest.cs
@@ -0,0 +1,63 @@
+﻿using Application.Handlers.Users;
+using Application.Interfaces;
+using Application.Queries;
+using Domain.Errors;
+using Domain.Models;
+using Moq;
+
+namespace Application.UnitTests.Handlers
+{
+    public class GetUserByIdHandlerTest
+    {
+        private readonly Mock<IUserRepository> _mockedUserRepository;
+
+        public GetUserByIdHandlerTest()
+        {
+            _mockedUserRepository = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnFailureResult_WhenUserNotFOund()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var query = new GetUserByIdQuery(id);
+
+            _mockedUserRepository.Setup(
+                x => x.GetById(id))
+                .ReturnsAsync(() => null);
+
+            var handler = new GetUserByIdHandler(_mockedUserRepository.Object);
+
+            //Act
+            var result = await handler.Handle(query, default);
+
+            //Assert
+            Assert.True(result.IsFailure);
+            Assert.Contains(
+                UserErrors.NotFound(id.ToString()), 
+                result.Errors);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnSucessResult_WhenUserExist()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var query = new GetUserByIdQuery(id);
+
+            _mockedUserRepository.Setup(
+                x => x.GetById(id))
+                .ReturnsAsync(new User());
+
+            var handler = new GetUserByIdHandler(_mockedUserRepository.Object);
+
+            //Act
+            var result = await handler.Handle(query, default);
+
+            //Assert
+            Assert.True(result.IsSucess);
+            Assert.IsType<User>(result.Data);
+        }
+    }
+}
