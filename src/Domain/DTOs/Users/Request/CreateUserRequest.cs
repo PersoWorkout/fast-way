363c777a1f09f9762f8346f177aa817e4b858493
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 14:33:34 +0100

    feat(domain.users): add create user request

---
+++ src/Domain/DTOs/Users/Request/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs.Users.Request
+{
+    public class CreateUserRequest
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
