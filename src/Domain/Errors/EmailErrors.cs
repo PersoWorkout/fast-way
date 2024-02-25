6e974243a7ea27c2b54589e614b169992054eb5b
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 14:34:16 +0100

    feat(domain): update user errors

--- src/Domain/Errors/EmailErrors.cs
+++ src/Domain/Errors/EmailErrors.cs
@@ -4,7 +4,7 @@ namespace Domain.Errors;
 
 public static class EmailErrors
 {
-    public static Error IsRequired = new("Email.IsRequired", "The email value is required");
-    public static Error InvalidEmail = new("Email.Invalid", "The value is not a valid email");
+    public static readonly Error Empty = new("Email.Empty", "'Email' must not be empty");
+    public static readonly Error Invalid = new("Email.Invalid", "'Email' is not a valid email address");
 }
 
--- src/Domain/Errors/PasswordErrors.cs
+++ src/Domain/Errors/PasswordErrors.cs
@@ -4,6 +4,6 @@ namespace Domain.Errors;
 
 public static class PasswordErrors
 {
-    public static Error IsRequired = new("Password.IsRequired", "The password is required");
-    public static Error IsInvalid = new("Password.Invalid", "The value is not a valid password");
+    public static readonly Error Empty = new("Password.Empty", "'Password' must be not empty");
+    public static readonly Error Invalid = new("Password.Invalid", "'Password' is not a valid password");
 }
