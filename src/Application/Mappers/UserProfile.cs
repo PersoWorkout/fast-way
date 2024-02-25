ab5d889a8b715c09847741ad427c4fedec9b2809
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 14:44:41 +0100

    feat(application.users): add request to command and command to user Map for creation

--- src/Application/Mappers/UserProfile.cs
+++ src/Application/Mappers/UserProfile.cs
@@ -1,6 +1,9 @@
-﻿using AutoMapper;
+﻿using Application.Commands.Users;
+using AutoMapper;
+using Domain.DTOs.Users.Request;
 using Domain.DTOs.Users.Response;
 using Domain.Models;
+using Domain.ValueObjects;
 
 namespace Application.Mappers
 {
@@ -8,9 +11,19 @@ namespace Application.Mappers
     {
         public UserProfile() 
         {
+            CreateMap<CreateUserRequest, CreateUserCommand>();
+
+            CreateMap<CreateUserCommand, User>()
+                .ForMember(dest => dest.Email,
+                    opt => opt.MapFrom(
+                        src => EmailValueObject.Create(src.Email).Data))
+                .ForMember(dest => dest.Password,
+                    opt => opt.MapFrom(
+                        src => PasswordValueObject.Create(src.Password).Data));
+
             CreateMap<User, UserForList>();
             CreateMap<User, UserDetails>()
-                .ForMember(src => src.Email,
+                .ForMember(dest => dest.Email,
                     opt => opt.MapFrom(
                         src => src.Email.Value));
 
