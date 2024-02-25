7f38131b1579a03a7a88c64e6a5546c7774713cf
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 10:46:33 +0100

    feat(application): add automapper & create user profile

---
+++ src/Application/Mappers/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.DTOs.Users.Response;
+using Domain.Models;
+
+namespace Application.Mappers
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<User, UserForList>();
+            CreateMap<User, UserDetails>()
+                .ForMember(src => src.Email,
+                    opt => opt.MapFrom(
+                        src => src.Email.Value));
+
+        }
+    }
+}
