0bfb0fa52ec3ea4d9773cbbe3cb5d7716d74e01c
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 10:39:56 +0100

    feat(domain.user): add user response dtos

---
+++ src/Domain/DTOs/Users/Response/UserDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs.Users.Response
+{
+    public class UserDetails
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/Domain/DTOs/Users/Response/UserForList.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs.Users.Response
+{
+    public class UserForList
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
