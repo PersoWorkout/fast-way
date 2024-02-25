b393d1992e0edfeae480c7957ccd1644e7bd557b
Yasin2719 <karakusyasin19@gmail.com>
2024-02-25 18:21:12 +0100

    feat(application.users): add update user command

---
+++ src/Application/Commands/Users/UpdateUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstractions;
+using Domain.DTOs.Users.Response;
+using Domain.ValueObjects;
+using MediatR;
+
+namespace Application.Commands.Users
+{
+    public class UpdateUserCommand: IRequest<Result<UserDetails>> 
+    {
+        public Guid? Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public EmailValueObject? Email { get; set; }
+        public PasswordValueObject? Password { get; set; }
+    }
+}
