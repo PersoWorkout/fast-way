a5f189cd7222be7b5b6496ed89286cdbcc9d3460
Yasin2719 <karakusyasin19@gmail.com>
2024-02-26 22:05:07 +0100

    feat(application.users): add & test delete user handler

---
+++ src/Application/Handlers/Users/DeleteUserHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Commands.Users;
+using Application.Interfaces;
+using Domain.Abstractions;
+using Domain.Errors;
+using MediatR;
+
+namespace Application.Handlers.Users
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, Result<object>>
+    {
+        private readonly IUserRepository _userRepository;
+        
+        public DeleteUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Result<object>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetById(request.Id);
+            if (user is null)
+                return Result<object>.Failure(
+                    UserErrors.NotFound(
+                        request.Id.ToString()));
+
+            await _userRepository.Delete(user);
+
+            return Result<object>.Success();
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Handlers/DeleteUSerHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using Application.Commands.Users;
+using Application.Handlers.Users;
+using Application.Interfaces;
+using Domain.Errors;
+using Domain.Models;
+using Moq;
+
+namespace Application.UnitTests.Handlers
+{
+    public class DeleteUSerHandlerTest
+    {
+        private readonly Mock<IUserRepository> _mockedUserRepository;
+        private readonly DeleteUserHandler _handler;
+
+        public DeleteUSerHandlerTest()
+        {
+            _mockedUserRepository = new Mock<IUserRepository>();
+            _handler = new(_mockedUserRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnFailureResult_WhenUserNotFound()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+
+            var command = new DeleteUserCommand(userId);
+
+            _mockedUserRepository.Setup(
+                x => x.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync(() => null);
+
+            //Act
+            var result = await _handler.Handle(command, default);
+
+            //Assert
+            Assert.True(result.IsFailure);
+            Assert.Contains(
+                UserErrors.NotFound(userId.ToString()),
+                result.Errors);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnResultFailure_WhenAllIsValid()
+        {
+            var userId = Guid.NewGuid();
+
+            var command = new DeleteUserCommand(userId);
+
+            _mockedUserRepository.Setup(
+                x => x.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync(new User());
+
+            //Act
+            var result = await _handler.Handle(command, default);
+
+            //Assert
+            Assert.True(result.IsSucess);
+        }
+    }
+}
