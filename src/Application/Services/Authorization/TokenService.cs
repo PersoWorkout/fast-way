ffc9c2335c6c53a25d9cf766f1bd3d8106aca4e1
Yasin2719 <karakusyasin19@gmail.com>
2024-02-27 21:06:37 +0100

    feat(auth): add & test token service

---
+++ src/Application/Services/Authorization/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace Application.Services.Authorization
+{
+    public static class TokenService
+    {
+        public static string Generate()
+        {
+            byte[] tokenData = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(tokenData);
+            }
+
+            return BitConverter.ToString(tokenData).Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Services/TokenServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services.Authorization;
+
+namespace Application.UnitTests.Services
+{
+    public class TokenServiceTest
+    {
+        [Fact]
+        public void Generate_ShouldBeGenerateAUniqueToken()
+        {
+            //Arrange
+            //Act
+            var firstToken = TokenService.Generate();
+            var secondToken = TokenService.Generate();
+
+            
+
+            Assert.NotEqual(firstToken, secondToken);
+        }
+    }
+}
