3d29a8095d2f1d0b458431b658821a6479f963a0
Yasin2719 <karakusyasin19@gmail.com>
2024-02-27 18:36:24 +0100

    feat(auth.domain): add AuthErrors class

---
+++ src/Domain/Errors/AuthErrors.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Errors
+{
+    public static class AuthErrors
+    {
+        public readonly static Error InvalidCreadentials = new(
+            "Authentication.InvalidCredantials", 
+            "The creadentials are not valid");
+    }
+}
