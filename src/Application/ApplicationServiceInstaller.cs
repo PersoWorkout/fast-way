ae0e333974cef45da088165483be38219f5ce32c
Yasin2719 <karakusyasin19@gmail.com>
2024-02-28 22:59:18 +0100

    feat(application): add service installer

---
+++ src/Application/ApplicationServiceInstaller.cs
@@ -0,0 +1,47 @@
+﻿using Application.Actions.Authorization;
+using Application.Actions.Users;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ApplicationServiceInstaller
+    {
+        public static IServiceCollection InstallApplicationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => 
+                cfg.RegisterServicesFromAssembly(
+                    Assembly.GetExecutingAssembly()));
+
+            services.InstallUserActions();
+
+            services.InstallAuthActions();
+
+            return services;
+        }
+
+        private static IServiceCollection InstallUserActions(this IServiceCollection services)
+        {
+            services.AddScoped<CreateUserAction>();
+            services.AddScoped<DeleteUserAction>();
+            services.AddScoped<GetUserByIdAction>();
+            services.AddScoped<GetUsersAction>();
+            services.AddScoped<UpdateUserAction>();
+
+            return services;
+        }
+
+        private static IServiceCollection InstallAuthActions(this IServiceCollection services)
+        {
+            services.AddScoped<LoginAction>();
+            services.AddScoped<RegisterAction>();
+            services.AddScoped<LogoutAction>();
+
+            return services;
+        }
+    }
+}
