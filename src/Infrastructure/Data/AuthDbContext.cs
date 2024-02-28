d405e61d1cc3097e81bb3b8b705670d70e37aa79
Yasin2719 <karakusyasin19@gmail.com>
2024-02-28 22:25:16 +0100

    feat(infrastructure): add auth db context

---
+++ src/Infrastructure/Data/AuthDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AuthDbContext: DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : 
+            base(options) { }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Session>()
+                .HasKey(x => new { x.UserId, x.Token });
+        }
+    }
+}
