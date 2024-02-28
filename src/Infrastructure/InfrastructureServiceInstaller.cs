7c10e3d6e042567ca23da99efc83b8ed2d4a2f96
Yasin2719 <karakusyasin19@gmail.com>
2024-02-28 22:43:00 +0100

    feat(infrastructure): add service installer

---
+++ src/Infrastructure/InfrastructureServiceInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServiceInstaller
+    {
+        public static IServiceCollection InstallInfrastructureServices(
+            this ServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(option =>
+                option.UseNpgsql(configuration
+                    .GetConnectionString("DatabaseConnection")));
+
+            services.AddDbContext<AuthDbContext>(option =>
+                option.UseInMemoryDatabase("AuthDatabase"));
+
+            return services;
+        }
+    }
+}
