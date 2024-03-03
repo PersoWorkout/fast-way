dbcfd04450a5f8e78fd4e0799399885879547034
Yasin2719 <karakusyasin19@gmail.com>
2024-03-03 11:20:56 +0100

    feat(auth): add authenticated attribute

---
+++ src/Presentation/Authorization/Attributes/AuthenticatedAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Presentation.Authentication.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthenticatedAttribute : Attribute, IAsyncAuthorizationFilter
+    {
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext
+                .Request
+                .Headers
+                .Authorization
+                .ToString();
+
+            var session = await context.HttpContext
+                .RequestServices
+                .GetRequiredService<IAuthorizationRepository>()
+                .GetByToken(token);
+
+            if (session is null)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            context.HttpContext.Items["userId"] = session.UserId.ToString();
+        }
+    }
+}
